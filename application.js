e47d74c370f17d6108eba43602e9095bddcd3edd
raveve <raven.askew@gmail.com>
2015-01-22 14:54:24 -0500

    Initial commit of files

---
+++ application.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function() {
+
+// EVENT 1
+
+$("h1").on("mouseover", function(){
+  console.log("change working");
+  $(this).css("background-color", "green");
+});
+
+//$('.box').transition({
+  //perspective: '100px',
+  //rotateY: '180deg'
+//});
+
+// EVENT 2
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 3
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 4
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 5
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 6
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 7
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 8
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 9
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+// EVENT 10
+
+//$("ELEMENT/SELECTION").on("EVENT NAME", function(){
+
+//});
+
+
+});
