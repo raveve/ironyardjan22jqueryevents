3a1e5fed0d9335f9b1d75ee4a5ec0902e44aa693
raveve <raven.askew@gmail.com>
2015-02-25 17:45:00 -0500

    Added comments to JS events and removed console.log

--- application.js
+++ application.js
@@ -1,76 +1,66 @@
 jQuery(document).ready(function() {
 
-// EVENT 1
+// EVENT 1 - When mouse enters any div on the page the header text fades in
 
 $("div").on("mouseenter", function(){
-  console.log("event 1 working");
   $("h1").fadeIn(1000);
 });
 
-// EVENT 2
+// EVENT 2 - When the mouse moves out of the 1st image (top left) body text slides in to their own divs
 $("img").eq(0).on("mouseout", function(){
-  console.log("event 2 working");
   $("p").slideDown(1000);
 });
 
-// EVENT 3
+// EVENT 3 - Double clicking on the second image (top row) places a border around said image
 
 $("img").eq(1).on("dblclick", function(){
-  console.log("event 3 working");
   $(this).css("border", "5px solid #9FB4CC")
 });
 
-// EVENT 4
+// EVENT 4 - Click on the first section of text (top row) changes the font color for said section
 
 $("p").eq(0).on("click", function(){
-  console.log("event 4 working");
   $(this).css("color", "#FFF8E3")
 });
 
-// EVENT 5
+// EVENT 5 - Clicking on the second section of text (middle row, first text section), upon mouse up click font weight goes from normal to bold
 
 $("p").eq(1).on("mouseup", function(){
-  console.log("event 5 working");
   $(this).css("font-weight", "bold");
 });
 
-// EVENT 6
+// EVENT 6 - When the browser window is resized the window width is logged to the console
 
 $(window).on("resize", function(){
-  console.log("event 6 working");
-  $("body").append("<div>" + $(window).width() + "</div>");
+  console.log($(window).width());
 
 });
 
-// EVENT 7
+// EVENT 7 - When mouse leaves the third image (first image, middle row) it animates to a width of 50% at a rate of 1000 ms
 
 $("img").eq(2).on("mouseleave", function(){
-  console.log("event 7 working");
   $(this).animate({ width: "50%" }, 1000);
 });
 
-// EVENT 8
+// EVENT 8 - On mouse moving over footer text / link it animates at a rate of 1000 ms over the width of 100% of the footer to the center where it belongs
 
 $("a").on("mouseover", function(){
-  console.log("event 8 working");
   $(this).animate({ width: "100%" }, 1000);
   console.log(event.relatedTarget.nodeName);
 });
 
-// EVENT 9
+// EVENT 9 - On mouse being pressed down on the third section of text (bottom row, center) the text has a line-through decoration and upon mouse being moved up the text decoration is removed
 
 $("p").eq(2).on("mousedown", function(){
-  console.log("event 9 working");
   $(this).css("text-decoration", "line-through");
   $(this).on("mouseup", function(){
   $(this).css("text-decoration", "none");
   });
 });
 
-// EVENT 10
+// EVENT 10 - When the mouse goes over the last image (bottom row, far right) it fades out in 3500 ms to nothing (hidden)
 
 $("img").eq(5).on("mouseover", function(){
-  console.log("event 10 working");
   $(this).fadeOut(3500);
 });
 
